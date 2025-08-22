5aeca55b247587102c76a4a16b42214bd200c922
ajned0089 <ammarjned71@gmail.com>
2025-08-22 08:32:13 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,10 +15,12 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
+API_ID = "27872897" #config("API_ID", default=None, cast=int)
+API_HASH = "c813ba7b7215484aa212f98ae1e8a686" #config("API_HASH", default=None)
+BOT_TOKEN = "8049480268:AAFOT516JlELurkeImSn7IB6u5P38SG6K44" #config("BOT_TOKEN", default=None)
+SESSION = "BAGpToEAKqhcXRUHgPNRVe2mlrYOnZVawk_VtN6VqzIYdtKhzFclH2HLubTmlzQ9_sdg_KImNvTLnctciw8Zgg-PpxO2SfZEkbGvWtEP1avIWA5hkf2npY01-Zucpy6zDi8GMxa_VOd6muy56_rruzBL-lHpLksIfYYaSn1om8zADoEdPn3fteQj7TJZUHNFg0x9i3PQJQiNeCf4lVKuQdHRDb4uG_ag26c70q71Kk_PtOusYfJRf7gtbS0TRkxJ8FCFHBeWb5T-lZqOYeyOv9kajNDjW10GMLRLeC7RWLl35l4FR_JNb2qnaJe3HtW-YV8Iw0pXbKksHaJYsJ6rb49tUxf25gAAAAHNev2vAA" #config("SESSION", default=None)
+FORCESUB = "" #config("FORCESUB", default=None)
+AUTH = "7742356911" #config("AUTH", default=None)
 FORCESUB = config("FORCESUB", default=None)
 AUTH = config("AUTH", default=None)
 SUDO_USERS = []
